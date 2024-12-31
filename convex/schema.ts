ba5e49a997bbef2aa973247c17058efbe82813ae
Abhishek Jaiswar <abhishekndxw@gmail.com>
2025-01-01 04:16:07 +0530

    ends up setting convex functions more likely tables

--- convex/schema.ts
+++ convex/schema.ts
@@ -22,12 +22,25 @@ export default defineSchema({
 
     snippets: defineTable({
         userId: v.string(), //clerkId,
-        email: v.string(),
-        name: v.string(),
-        isPro: v.boolean(),
-        proSinceBuilt: v.optional(v.number()),
-        lemonSqueezyCustomerId: v.optional(v.string()),
-        lemonSqueezyOrderId: v.optional(v.string())
-    }).index("by_user_id", ["userId"]),
+        title: v.string(),
+        code: v.string(),
+        username: v.string(),
+        language: v.string(),
+    }).index("by_user_id", ["userId"]), //we could find things by using this alias
+
+    snippetComments: defineTable({
+        snippetId: v.id("snippets"), //clerkId,
+        userId: v.string(),
+        username: v.string(),
+        content: v.string(), //This will store html contnet maybe for structure
+    }).index("by_snippet_id", ["snippetId"]),
+
+    stars: defineTable({
+        snippetId: v.id("snippets"), //clerkId,
+        userId: v.id("users"), //clerkId,
+    })
+    .index("by_user_id", ["userId"])
+    .index("by_snippet_id", ["snippetId"])
+    .index("by_user_id_and_snippet_id", ["userId", "snippetId"]),
 });
 
